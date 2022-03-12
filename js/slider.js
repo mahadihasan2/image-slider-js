654920c822c5dd0c4b9ed9a151b32defbbba42a4
mahadihasan2 <mahadihasan125643@gmail.com>
2022-03-12 18:26:24 +0600

    image-slider-done

---
+++ js/slider.js
@@ -0,0 +1,24 @@
+const images = [
+    'image/image-01.jpg',
+    'image/image-02.jpg',
+    'image/image-03.jpg',
+    'image/image-04.jpg',
+    'image/image-05.jpg',
+    'image/image-06.jpg',
+    'image/image-07.jpg',
+    'image/image-08.jpg',
+    'image/image-09.jpg'
+];
+let imageid = 0;
+const imageElement = document.getElementById('img-src')
+setInterval(() => {
+    if (imageid >= images.length) {
+        imageid = 0;
+    }
+    // console.log(imageid)
+    const imgurl = images[imageid]
+    imageElement.setAttribute('src', imgurl)
+    // console.log(imgurl);
+    imageid++;
+
+}, 1000)
